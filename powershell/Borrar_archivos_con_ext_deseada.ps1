19b6a12d6567a5773d50ff944e519b5018cdb1ea
TeodoroAnelloasix2 <2023_teodoro.anello@iticbcn.cat>
2025-02-09 20:28:06 +0100

    Some powershell

---
+++ powershell/Borrar_archivos_con_ext_deseada.ps1
@@ -0,0 +1,38 @@
+﻿param(
+    [string]$directorio="C:\Users\giaco",
+    [string]$ext=".txt"
+)
+
+
+$logs=Get-childitem -Path $directorio -Recurse -Filter "*$ext"
+$logCount= $logs.Count
+$totalSize=($logs | Measure-Object -Property Length -Sum).sum
+$totalSizeMB=[math]::Round($totalSize/1MB,2)
+
+$oldest= $logs | Sort-Object LastWriteTime | Select-Object -First 1
+$newest= $logs | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+
+@"
+
+   Crear comentario multilínea 
+"@
+
+$text=@"
+    Cantidad de archivos encontrados: $logcount
+    Tamaño total: $totalSizeMB MB
+    Mas reciente: $($newest.Lastwritetime) $newest
+    Mas antiguo: $($oldest.Lastwritetime) $oldest
+
+"@
+$text | out-file -FilePath "$directorio\resumen.txt"
+
+$oldlogs= $logs | where-object { $_.LastWriteTime -lt (Get-Date).AddDays(-30)}
+
+foreach ($log in $oldlogs){
+Get-Content $log | Write-Output 
+Remove-Item $log.fullname -Confirm }
+
+
+Get-Content "$directorio\resumen.txt" | Write-Output
+
+#Get-Content "test.txt" | Write-Output
---
+++ powershell/backup.ps1
@@ -0,0 +1,35 @@
+﻿param(
+    [string]$source="C:\Users\giaco\OneDrive\Desktop\asix2\powershell",
+    [string]$dest="C:\Users\giaco\OneDrive\Desktop\asix2\powershell\backups",
+    [int]$days=5
+
+)
+
+#Crear carpeta de backup con marca de tiempo
+
+$timestamp=Get-date -Format "ddMMyyyy"
+$backupdir="$dest\Backup_$timestamp"
+
+if(!(Test-path $backupdir)){ New-Item -ItemType directory -path $backupdir }
+
+$recentFiles=get-childitem -path $source -recurse | where-object {
+    $_.LastWriteTime -ge (Get-Date).AddDays(-$days)
+}
+
+$log=@()
+
+
+foreach ($file in $recentFiles){
+    
+    $destPath="$backupdir\$($file.name)"
+    copy-item -path $file.fullname -Destination $destPath
+    #Agerar archivo copiado a log
+
+    $log+="Copiando: $($file.Fullname) ---> $destpath"
+}
+
+$log | out-file -filepath "$backupdir\informe_backup.txt"
+
+
+
+$log | ForEach-Object { Write-Output $_ }
